dd3b965399f65ddf5bae7f40cd8ae9d5394f8e52
Denis Oliveira <denis.9697@gmail.com>
2023-10-22 14:43:02 -0300

    Primeiro Commit

---
+++ Dio5/src/main/java/me/dio/Dio5/Dio5Application.java
@@ -0,0 +1,13 @@
+package me.dio.Dio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dio5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dio5Application.class, args);
+	}
+
+}
---
+++ Dio5/src/test/java/me/dio/Dio5/Dio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Dio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
